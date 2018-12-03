bb51cd873e8f8585f4038229bddf2126a5992690
Eduardo Neis <eduardo.neis@gmail.com>
2018-12-03 15:16:09 -0200

    populacao gerada com sucesso

--- scratch.py
+++ scratch.py
@@ -1,11 +1,11 @@
 import random
 
-def geraPopulacaoinicial(tamanhodapop):
-    return [geraCromossomo() for x in range (0, tamanhodapop)]
+def geraPopulacaoinicial(tamanhodapop, qtdItens):
+    return [geraCromossomo(qtdItens) for x in range (0, tamanhodapop)]
 
 
-def geraCromossomo():
-    return [random.randint(0, 1) for x in range(0, len(ITEMS))]
+def geraCromossomo(qtdItens):
+    return [random.randint(0, 1) for x in range(0, qtdItens)]
 
 
 
@@ -21,8 +21,8 @@ def main():
     PESO_OBJ_MIN = int(input('Insira qual deve ser o peso minimo dos objetos '))
     VALOR_OBJ_MAX = int(input('Insira qual deve ser o valor maximo dos objetos: '))
     VALOR_OBJ_MIN = int(input('Insira qual deve ser o valor minimo dos objetos: '))
-   geracao = int(1)
-   populacao = geraPopulacaoinicial(TAM_POP) #gera nova população
-
+    geracao = 1
+    populacao = geraPopulacaoinicial(TAM_POP, QuantidadeItens) #gera nova população
+    print(populacao)
 
 main()
