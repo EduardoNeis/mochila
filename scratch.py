f993ec39993e659fccfd00b86511897f5ea8f123
Eduardo Neis <eduardo.neis@gmail.com>
2018-12-03 13:46:22 -0200

    rascunho do input na funcao main e prototipagem da geracao de populacao

--- scratch.py
+++ scratch.py
@@ -1,11 +1,28 @@
-QuantidadeItens = int(input('Insira a quantidade de itens: '))
-x=0
-PesoItem = []
-ValorItem = []
-while x < QuantidadeItens:
-    PesoItem.append(int(input('Insira o peso do item: ')))
-    ValorItem.append(int(input('Insira o valor do item: ')))
-    x +=1
+import random
 
-print(PesoItem)
-print(ValorItem)
+def geraPopulacaoinicial(tamanhodapop):
+    return [geraCromossomo() for x in range (0, tamanhodapop)]
+
+
+def geraCromossomo():
+    return [random.randint(0, 1) for x in range(0, len(ITEMS))]
+
+
+
+
+def main():
+    QuantidadeItens = int(input('Insira qual deve ser a quantidade de itens: '))
+    TAM_POP = int(input('Insira qual deve ser o tamanho da população: '))
+    QTD_ITE = int(input('Insira qual deve ser o numero de iteracoes: '))
+    intervalo = int(input('Insira qual deve ser o intervalo de geracoes: '))
+    taxa = int(input('Insira qual deve ser a taxa de mutacoes: '))
+    CAPACIDADE_MOCHILA = int(input('Insira qual deve ser o peso maximo da mochila: '))
+    PESO_OBJ_MAX = int(input('Insira qual deve ser o peso maximo dos objetos: '))
+    PESO_OBJ_MIN = int(input('Insira qual deve ser o peso minimo dos objetos '))
+    VALOR_OBJ_MAX = int(input('Insira qual deve ser o valor maximo dos objetos: '))
+    VALOR_OBJ_MIN = int(input('Insira qual deve ser o valor minimo dos objetos: '))
+   geracao = int(1)
+   populacao = geraPopulacaoinicial(TAM_POP) #gera nova população
+
+
+main()
