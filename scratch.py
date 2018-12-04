65553f2596ca9bb2c9200ea9e2c54ec0f9e02e29
Eduardo Neis <eduardo.neis@gmail.com>
2018-12-04 13:31:53 -0200

    Prototipo da funcao fitness, prototipo da funcao de gerar items

--- scratch.py
+++ scratch.py
@@ -1,5 +1,11 @@
 import random
 
+class Item(object):
+    def __init__(self, v, p):
+        self.valor = v
+        self.peso = p
+
+
 def geraPopulacaoinicial(tamanhodapop, qtdItens):
     return [geraCromossomo(qtdItens) for x in range (0, tamanhodapop)]
 
@@ -8,7 +14,8 @@ def geraCromossomo(qtdItens):
     return [random.randint(0, 1) for x in range(0, qtdItens)]
 
 
-
+def geraItens():
+    ITEMS = [Item(random.randint(VALOR_OBJ_MIN, VALOR_OBJ_MAX), random.randint(PESO_OBJ_MIN, PESO_OBJ_MAX))]
 
 def main():
     QuantidadeItens = int(input('Insira qual deve ser a quantidade de itens: '))
@@ -29,11 +36,28 @@ def main():
         populacao = sorted(populacao, key=lambda x: fitness(x), reverse=True)
         for i in populacao:
             print('{} fitness: {}'.format(str(i), fitness(i)))
-            populacao =
+            populacao = evolutiva(populacao)
             geracao += 1
 
     print(populacao)
 
+def fitness():
+    indice = 0
+    for i in target:
+        if indice >= QuantidadeItens11:
+            break
+        if (i == 1):
+            valor_total +=
+            peso_total +=
+        indice += 1
+
+    if peso_total > CAPACIDADE_MOCHILA
+        return 0 #se o peso total for maior que a capacidade da mochila fitness = 0
+    else
+        return valor_total
+
+
+def evolutiva():
 
 
 main()
